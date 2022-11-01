91205601637eafbaecbc904125c56e32c33732d2
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-01 22:48:06 +0100

    Added main App component
    Template and style are done, all that is left is
    to finish the script tag content.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            text: 'Check the console 👀'
+        }
+    },
+    mounted() {
+        console.log('%c ⬆️ ⬇️ ⬆️ ⬇️ ⬅️ ➡️ ⬅️ ➡️ B A ENTER', 'font-size: 1.5rem');
+    }
+}
+</script>
+
+<template>
+    <p>{{text}}</p>
+</template>
+
+<style scoped>
+    p {
+        font-size: 5rem;
+    }
+</style>
