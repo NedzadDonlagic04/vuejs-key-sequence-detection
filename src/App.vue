14297d6d3090799504ff8d939627639c2b76da68
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-01 23:30:38 +0100

    Added event listener to track konami code

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,33 @@
 export default {
     data() {
         return {
-            text: 'Check the console 👀'
+            i: 0,
+            code: ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 
+                   'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight',
+                   'b', 'a', 'Enter']
         }
     },
+    computed: {
+        text() {
+            if(this.i === this.code.length) {
+                this.i = 0;
+                return 'Konami Code Entered!';
+            }   
+            return 'Check the console 👀';
+        }
+    },  
     mounted() {
         console.log('%c ⬆️ ⬆️ ⬇️ ⬇️ ⬅️ ➡️ ⬅️ ➡️ B A ENTER', 'font-size: 1.5rem');
+
+        window.addEventListener('keyup', event => {
+            const { key } = event;
+            
+            if(key === this.code[this.i]) {
+                this.i++;
+            } else {
+                this.i = 0;
+            }
+        });
     }
 }
 </script>
